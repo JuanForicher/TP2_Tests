05f8820b266993a1ff27b0cc6536b019aefe62c5
Ramiro <gran.ranmi08@gmail.com>
2026-09-21 22:28:11 -0300

    feat: ejercicio 5 (verde)

--- src/services/NoteService.ts
+++ src/services/NoteService.ts
@@ -47,7 +47,6 @@ export class NoteServiceImpl implements NoteService {
   }
 
   deleteNote(id: number): boolean {
-    // 🔴🟢 EJERCICIO 5: ciclo completo.
-    throw new Error('deleteNote: no implementado (Ejercicio 5)');
+    return this.repo.delete(id);
   }
 }
